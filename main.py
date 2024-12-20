cd54c056abf9ec138862757bf4dcec9732c2498e
Nikiliks <nktalpeev@gmail.com>
2024-12-20 11:45:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def general() -> dict:
+async def general() -> str:
     return {"message": "Главная страница"}
 
 @app.get("/user/admin")
-async def admin() -> dict:
+async def admin() -> str:
     return {"message": f"Вы вошли как администратор"}
 
 @app.get("/user/{user_id}")
-async def id_call(user_id: int) -> dict:
+async def id_call(user_id: int) -> str:
     return {"message": f"Вы вошли как пользователь №{user_id}"}
 
 @app.get("/user")
-async def user_call(username: str, age: int) -> dict:
+async def user_call(username: str, age: int) -> str:
     return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
 
