378237ab371583bbe5380c4ea1eea385f24fa8d9
Nikiliks <nktalpeev@gmail.com>
2024-12-21 08:40:05 +0500

    Update now main.py

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ app = FastAPI()
 
 @app.get("/")
 async def general() -> str:
-    return {"message": "Главная страница"}
+    return "Главная страница"
 
 @app.get("/user/admin")
 async def admin() -> str:
-    return {"message": f"Вы вошли как администратор"}
+    return "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
 async def id_call(user_id: int) -> str:
-    return {"message": f"Вы вошли как пользователь №{user_id}"}
+    return f"Вы вошли как пользователь №{user_id}"
 
 @app.get("/user")
 async def user_call(username: str, age: int) -> str:
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
-
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
